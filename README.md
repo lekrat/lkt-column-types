fb878f608882917f7148c426a574fc5ea9d29e26
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2022-11-04 22:02:17 +0100

    Brand new world

---
+++ README.md
@@ -0,0 +1,5 @@
+# LKT Factory Types
+
+Currently on development, installation not recommended.
+
+Tiny package, just for enum the types of columns supported by LKT Factory.
---
+++ src/Enums/ColumnType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lkt\ColumnTypes\Enums;
+
+class ColumnType
+{
+    const String = 0;
+    const Integer = 1;
+    const Float = 2;
+    const Boolean = 3;
+    const HTML = 4;
+    const Email = 5;
+    const ForeignKey = 6;
+    const Datetime = 7;
+    const UnixTimeStamp = 8;
+    const Image = 9;
+    const File = 10;
+    const JSON = 11;
+    const Pivot = 12;
+    const Related = 13;
+    const Color = 14;
+    const ForeignKeys = 15;
+    const RelatedKeys = 16;
+}
